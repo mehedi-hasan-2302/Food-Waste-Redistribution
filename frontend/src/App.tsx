8f62d6274d73c4c94fbb29d72fee33fa1f14044e
IslamTamjid <towhidulislam24@protonmail.com>
2025-05-27 07:32:05 +0600

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from "@/components/ui/button";
+
+function App() {
+
+  return (
+    <>
+     <div className='text-orange-500 bg-black'>Hello World</div>
+     <div><Button variant={"outline"}>Click Me</Button></div>
+    </>
+  )
+}
+
+export default App
