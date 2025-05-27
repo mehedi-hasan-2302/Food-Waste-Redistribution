e398312a9d7ee0224c13c07d29d1db7416bffe83
IslamTamjid <towhidulislam24@protonmail.com>
2025-05-27 07:58:21 +0600

    add colors scheme and fonts combination

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-     <div className='text-orange-500 bg-black'>Hello World</div>
+     <div className='text-orange-500 bg-pale-mint text-3xl font-sans'>Hello World</div>
      <div><Button variant={"outline"}>Click Me</Button></div>
     </>
   )
