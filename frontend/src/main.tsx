80d1fe74b50b662f71ab84b6926972719c07c7e7
IslamTamjid <towhidulislam24@protonmail.com>
2025-05-29 13:32:26 +0600

    feat(Routing): add routing between components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,51 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import './index.css'
-import App from './App.tsx'
+import RootLayout from './Pages/RootLayout.tsx'
+import Home from './Pages/primary/Home.tsx';
+import Services from './Pages/primary/Services.tsx';
+import About from './Pages/primary/About.tsx';
+import Contact from './Pages/primary/Contact.tsx';
+import LoginPage from './Pages/Auth/LoginPage.tsx';
+import SignupPage from './Pages/Auth/signup/SignupPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true, // This makes HomePage the default child route for "/"
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "services",
+        element: <Services />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "signup",
+        element: <SignupPage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
