d1e585a056a241efcdd715d13da68043e71712dc
IslamTamjid <towhidulislam24@protonmail.com>
2025-05-29 11:32:29 +0600

    refactor: moved LoginPage component for better organization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar';
 //import { Button } from "@/components/ui/button";
-import LoginPage from './Pages/LoginPage';
+import LoginPage from './Pages/Auth/LoginPage';
 
 function App() {
 
