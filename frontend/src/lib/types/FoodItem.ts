4860226dc0afffbee9e03a48dc02ab8a57c6d8bb
IslamTamjid <towhidulislam24@protonmail.com>
2025-06-02 06:08:46 +0600

    add sample data for checking food display details

---
+++ frontend/src/lib/types/FoodItem.ts
@@ -0,0 +1,15 @@
+export interface FoodItem {
+    Title: string;
+    Description: string;
+    FoodType: string;
+    CookedDate: string;
+    PickupWindowStart: string;
+    PickupWindowEnd: string;
+    PickupLocation: string;
+    IsDonation: boolean;
+    Price: number;
+    Quantity: string;
+    DietaryInfo: string;
+    image: string;
+    id: string;
+}
