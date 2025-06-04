6ac69cfb6717085e0ecfd763334a2364514a6aa7
IslamTamjid <towhidulislam24@protonmail.com>
2025-06-04 17:05:40 +0600

    add toast container over every page of the application

--- frontend/src/Pages/RootLayout.tsx
+++ frontend/src/Pages/RootLayout.tsx
@@ -1,15 +1,28 @@
 import Navbar from "@/components/Navbar"
 import { Outlet } from "react-router-dom"
+import { ToastContainer } from "react-toastify";
 
 const RootLayout: React.FC = () => {
     return (
-        <>
-            <Navbar />
-            <main>
-                <Outlet />
-            </main>
-        </>
-    )
+      <>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <Navbar />
+        <main>
+          <Outlet />
+        </main>
+      </>
+    );
 }
 
 export default RootLayout;
