fe806c6132e71f1089a8e0f78b2fa51002a2fbba
IslamTamjid <towhidulislam24@protonmail.com>
2025-06-05 11:39:27 +0600

    add state management attributes for profile authorization and recovering password

--- frontend/src/store/authStore.ts
+++ frontend/src/store/authStore.ts
@@ -23,13 +23,16 @@ interface AuthState {
   token: string | null;
   loginError: string | null;
   signupError: string | null;
+  passwordResetError: string | null;
   isLoading: boolean;
   isAuthenticated: () => boolean;
   loginSuccess: (userDataFromApi: UserPrimaryData, token: string) => void;
   logout: () => void;
   setLoginError: (error: string | null) => void;
   setSignupError: (error: string | null) => void;
+  setPasswordResetError: (error: string | null) => void;
   setIsLoading: (loading: boolean) => void;
+  updateProfileCompletionStatus: (isComplete: boolean) => void;
   //method for updating isProfileComplete later
   //updateUserProfileStatus: (isComplete: boolean) => void;
 }
@@ -41,6 +44,7 @@ export const useAuthStore = create<AuthState>()(
       token: null,
       loginError: null,
       signupError: null,
+      passwordResetError: null,
       isLoading: false,
       isAuthenticated: () => !!get().token && !!get().user,
       loginSuccess: (userDataFromApi: UserPrimaryData, token: string) => {
@@ -55,14 +59,31 @@ export const useAuthStore = create<AuthState>()(
         set({ user: appUser, token, loginError: null, isLoading: false });
       },
       logout: () =>
-        set({ user: null, token: null, loginError: null, isLoading: false }),
+        set({
+          user: null,
+          token: null,
+          loginError: null,
+          isLoading: false,
+          passwordResetError: null,
+          signupError: null,
+        }),
       setLoginError: (error: string | null) =>
         set({ loginError: error, isLoading: false }),
       setSignupError: (
         error: string | null // New: Implement setSignupError
-      ) => set({ signupError: error, isLoading: false }), 
+      ) => set({ signupError: error, isLoading: false }),
+      setPasswordResetError: (
+        error: string | null // New: Implement
+      ) => set({ passwordResetError: error, isLoading: false }),
       setIsLoading: (loading: boolean) => set({ isLoading: loading }),
+
+      updateProfileCompletionStatus: (isComplete: boolean) =>
+        set((state) => ({
+          user: state.user ? { ...state.user, isProfileComplete: isComplete } : null,
+        })),
+
     }),
+    
     {
       name: "auth-session-storage",
       storage: {
